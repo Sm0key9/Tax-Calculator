e98c46b063dee9432b5e802cb8c2b307fa85ac32
Sm0key9 <89950112+Sm0key9@users.noreply.github.com>
2023-05-09 15:00:25 +1000

    d
    d

--- Assets/Scripts/BackgroundChange.cs
+++ Assets/Scripts/BackgroundChange.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackgroundChange : MonoBehaviour
 {
 
+    public TMP_Dropdown backgrounddrop;
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
+
+    
 }
